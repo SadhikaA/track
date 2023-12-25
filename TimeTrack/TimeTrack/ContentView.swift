88eff60ca42fbc0682ab6dfd3b18a02127ac50e0
Sadhika Akula <sadhika.akula@berkeley.edu>
2023-12-24 23:29:31 -0800

    starting time track

---
+++ TimeTrack/TimeTrack/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TimeTrack
+//
+//  Created by Sadhika Akula on 12/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("welcome to timetrack!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TimeTrack/TimeTrack/TimeTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTrackApp.swift
+//  TimeTrack
+//
+//  Created by Sadhika Akula on 12/24/23.
+//
+
+import SwiftUI
+
+@main
+struct TimeTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
